d4bdb69334bf995230b1f4e16c07807518122b2e
Abdulloh <abdullohyoqubjonov110@gmail.com>
2025-11-08 23:00:55 +0500

    zakovat boshlangich

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,79 +1,32 @@
-// import { BASE_URL } from "@/api/baseUrl";
-// import { useEffect, useState } from "react"
-
-// type Param =  "documentation" | `questions${string}` | "categories";
-
-// function useFetch<T>(param: Param) {
-//     const [data, setData] = useState<T | null>(null);
-//     const [error, setError] = useState<string | null>(null);
-//     const [loading, setLoading] = useState<boolean>(false);
-
-//     async function fetchData() {
-//         setLoading(true);
-//         try {
-//             const response = await fetch(BASE_URL + param);
-//             if(!response.ok){
-//                 throw new Error("Xatolik bor: " + response.status);
-//             }
-//             const data = await response.json();
-//             setData(data);
-//         } catch (error: any) {
-//             setError(error.message);
-//         } finally {
-//             setLoading(false);
-//         }
-//     }
-//     useEffect(()=> {
-//         fetchData();
-//     }, []); 
-//   return { data, error, loading};
-// }
-
-// export default useFetch;
-
-
 import { BASE_URL } from "@/api/baseUrl";
-import { useEffect, useState } from "react";
+import { useEffect, useState } from "react"
 
-type Param = "documentation" | `questions${string}` | "categories";
+type Param =  "documentation" | `questions${string}` | "categories";
 
 function useFetch<T>(param: Param) {
-  const [data, setData] = useState<T | null>(null);
-  const [error, setError] = useState<string | null>(null);
-  const [loading, setLoading] = useState<boolean>(false);
+    const [data, setData] = useState<T | null>(null);
+    const [error, setError] = useState<string | null>(null);
+    const [loading, setLoading] = useState<boolean>(false);
 
-  async function fetchData() {
-    setLoading(true);
-    try {
-      console.log("Fetching:", BASE_URL + param);
-
-      
-      const url =
-        BASE_URL.endsWith("/") && param.startsWith("/")
-          ? BASE_URL + param.slice(1)
-          : BASE_URL + param;
-
-      const response = await fetch(url);
-
-      if (!response.ok) {
-        throw new Error("Xatolik bor: " + response.status);
-      }
-
-      const data = await response.json();
-      setData(data);
-    } catch (error: any) {
-      setError(error.message);
-    } finally {
-      setLoading(false);
+    async function fetchData() {
+        setLoading(true);
+        try {
+            const response = await fetch(BASE_URL + param);
+            if(!response.ok){
+                throw new Error("Xatolik bor: " + response.status);
+            }
+            const data = await response.json();
+            setData(data);
+        } catch (error: any) {
+            setError(error.message);
+        } finally {
+            setLoading(false);
+        }
     }
-  }
-
-  useEffect(() => {
-    fetchData();
-    
-  }, [param]);
-
-  return { data, error, loading };
+    useEffect(()=> {
+        fetchData();
+    }, []); 
+  return { data, error, loading};
 }
 
 export default useFetch;
