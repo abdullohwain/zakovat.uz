0105491b42d0011d036dac8101f981d87880d4df
Abdulloh <abdullohyoqubjonov110@gmail.com>
2025-11-08 16:01:35 +0500

    zakovat boshlangich

--- src/pages/GameField.tsx
+++ src/pages/GameField.tsx
@@ -4,66 +4,72 @@ import useFetch from "@/hooks/useFetch";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
 
-interface Question {
-  id: number;
-  word: string;
-  question: string;
+interface Question{
+    id:number
+    answer:string
+    question:string
 }
 
 interface Data {
-  id: number;
-  name: string;
-  questions: Question[];
+    id:number
+    name:string
+    questions:Question[]
 }
 
-const keyboard: string[] = ["qwertyuiop", "asdfghjkl", "zxcvbnm"];
+const keyboard:string[] = [
+    "qwertyuiop",
+    "asdfghjkl",
+    "zxcvbnm"
+]
 
 function GameField() {
-  const { id } = useParams();
-  const { data, loading } = useFetch<Data>(`questions/${id}`);
-  const [activeCuestion, setActiveCuestion] = useState(0);
 
-  if (loading) {
-    return <Loading />;
-  }
+    const {id} = useParams()
+    const {data, loading} = useFetch<Data>(`questions/${id}`)
+    const [activeQuestions, setActiveQuestions] = useState(0)
 
+
+    if(loading){
+        return <Loading/>
+    }
   return (
     <div className="container py-10">
-      <h2 className="text-2xl font-bold text-center mb-10">
-        {data?.questions?.[0]?.question}
-      </h2>
+        <h2 className="text-2xl font-bold text-center mb-10">{data && data?.questions[0].question}</h2>
 
-      <div className="flex flex-wrap gap-x-5 gap-y-10 items-center justify-center mb-20">
-        {data?.questions?.[0]?.word
-          ?.toUpperCase()
-          ?.split(" ")
-          ?.map((word, i) => (
-            <div className="flex" key={i}>
-              {word.split("").map((letter, j) => (
-                <span className="w-10 h-10 border border-gray-400" key={j}>
-                  {false && letter}
-                </span>
-              ))}
+        <div className="flex gap-5 items-center justify-center mb-20">
+            {data &&
+             data.questions[activeQuestions].answer
+            .toUpperCase()
+            .split(" ")
+            .map((answer,i)=>{
+                return(
+                    <div key={i}>{answer.split("").map((letter,i)=> {
+                        return (
+                            <span className="border border-gray-400 py-4 px-4" key={i}>
+                                {letter}
+                            </span>
+                        )
+                    })}</div>
+                )
+                
+            })}
+        </div>
+
+
+    <div className="flex flex-col gap-6 items-center">
+        {keyboard.map((str)=>{
+            return (<div key={str} className="flex items-center gap-4">
+                {str.toUpperCase().split("").map((key)=>{
+                    return (
+                    <Button key={key} className="text-xl font-bold">{key}</Button>
+                    )
+                })}
             </div>
-          ))}
-      </div>
-
-      <div className="flex flex-col gap-6 items-center">
-        {keyboard.map((str) => (
-          <div key={str} className="flex items-center gap-4">
-            {str
-              .toUpperCase()
-              .split("")
-              .map((key) => (
-                <Button key={key} className="text-xl font-bold">
-                  {key}
-                </Button>
-              ))}
-          </div>
-        ))}
-      </div>
+            )
+        })}
     </div>
-  );
+    </div>
+  )
 }
 
-export default GameField;
+export default GameField
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,31 +1,33 @@
 import Categories from '@/pages/Categories'
 import Document from '@/pages/Document'
-import GameField from '@/pages/GameField';
+import GameField from '@/pages/GameField'
 import Home from '@/pages/Home'
-import { createBrowserRouter,  RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 function Router() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home />,
-    },
-    {
-      path: "/instruction",
-      element: <Document />,
-    },
-    {
-      path: "/categories",
-      element: <Categories />,
-    },
-    {
-      path: "/game-field/:id",
-      element: <GameField />,
-    }
-    
-  ]);
-  return <RouterProvider router={router}></RouterProvider>;
-  
+
+    const router = createBrowserRouter([
+        {
+            path:"/",
+            element: <Home/>
+        },
+        {
+            path:"/instruction",
+            element: <Document/>
+        },
+        {
+            path:"/categories",
+            element: <Categories/>
+        },
+        {
+            path:"/game-field/:id",
+            element: <GameField/>
+        },
+    ])
+  return (
+    <RouterProvider router={router}>
+    </RouterProvider>
+  )
 }
 
 export default Router
